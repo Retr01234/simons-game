0726c6b83d0a09b6874ee158841a458fb09f4c39
Pave <pitcheroty@gmail.com>
2023-07-24 17:14:03 +0200

    feature: add test cases

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,31 @@
 /**
  * @jest-environment jsdom
  */
+
+const { game } = require("../game");
+
+// Rendering our HTML File into the DOM
+beforeAll(() => {
+  let fs = require("fs");
+  let fileContents = fs.readFileSync("index.html", "utf-8");
+  document.open();
+  document.write(fileContents);
+  document.close();
+});
+
+// Running our Test to see if the Score Chart works
+describe("game object contains correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true);
+  });
+  // Testing to see if our Game Key is there
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+  test("playerMovess key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+});
